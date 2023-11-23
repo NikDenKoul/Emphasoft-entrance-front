5d476d9d6f3db2d02c42e1a5e0c54c128c5ad7c1
centurion <niksoup@list.ru>
2023-11-23 05:45:56 +0300

    User profile loader

--- src/users/[id]/index.js
+++ src/users/[id]/index.js
@@ -4,8 +4,8 @@ import {useContext, useEffect, useState} from "react";
 import {AppContext} from "../../AppContext";
 import axios from "axios";
 import {Check, Cancel} from "../../components/icons";
-import '../index.css';
 import {SERVER_PATH, getRequestOptions} from '../../Utils';
+import '../index.css';
 
 function ProfileElement({label, value, type}) {
     return (
@@ -26,21 +26,27 @@ function UserPage() {
     const match = useMatch('/users/:id');
     const userId = match.params.id;
     const [user, setUser] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
     const {token} = useContext(AppContext);
 
     const fetchUser = () => {
         if (!token || !userId) return;
-        const requestOptions = getRequestOptions('get', token);
-        axios(`${SERVER_PATH}users/${userId}/`, requestOptions)
-            .catch((e) => ({ error: e.code, errorMessage: e.message }))
-            .then((response) => {
-                if (response.error) {
-                    console.error(response.error);
-                    return;
-                }
+        setIsLoading(true);
+        setTimeout(() => {
+            const requestOptions = getRequestOptions('get', token);
+            axios(`${SERVER_PATH}users/${userId}/`, requestOptions)
+                .catch((e) => ({ error: e.code, errorMessage: e.message }))
+                .then((response) => {
+                    if (response.error) {
+                        console.error(response.error);
+                        setIsLoading(false);
+                        return;
+                    }
 
-                setUser(response.data);
-            });
+                    setUser(response.data);
+                    setIsLoading(false);
+                });
+        }, 1000);
     }
 
     useEffect(() => {
@@ -49,15 +55,21 @@ function UserPage() {
 
     return (
         <AppLayout>
-            <h3>User №{userId}</h3>
-            <div className='profile-data'>
-                <ProfileElement label='Username' value={user.username} />
-                <ProfileElement label='First name' value={user.first_name} />
-                <ProfileElement label='Last name' value={user.last_name} />
-                <ProfileElement label='Is active' value={user.is_active} type='boolean' />
-                <ProfileElement label='Last login' value={user.last_login} type='date' />
-                <ProfileElement label='Is superuser' value={user.is_superuser} type='boolean' />
-            </div>
+            {!isLoading ? (
+                <>
+                    <h3>User №{userId}</h3>
+                    <div className='profile-data'>
+                        <ProfileElement label='Username' value={user.username} />
+                        <ProfileElement label='First name' value={user.first_name} />
+                        <ProfileElement label='Last name' value={user.last_name} />
+                        <ProfileElement label='Is active' value={user.is_active} type='boolean' />
+                        <ProfileElement label='Last login' value={user.last_login} type='date' />
+                        <ProfileElement label='Is superuser' value={user.is_superuser} type='boolean' />
+                    </div>
+                </>
+            ) : (
+                <div className='loader-outer'><div className='loader-inner'></div></div>
+            )}
         </AppLayout>
     )
 }
